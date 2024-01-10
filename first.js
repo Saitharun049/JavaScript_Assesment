989cb2309e7d409c8604a4689fae8b391998cbf4
saitharun <suthapalli.tharun@kellton.com>
2024-01-10 14:23:57 +0530

    first commit

--- first.js
+++ first.js
@@ -165,11 +165,55 @@
 
 // ------------------------------------------Function Expression--------------------------------
 
-let add = function(num1,num2)
-{
-    return num1 + num2
+// let add = function(num1,num2)
+// {
+//     return num1 + num2
     
+// }
+// let sum = add
+// let result =sum(5,5)
+// console.log(result);
+
+// ------------------------------ Multipe Values in function------------------------------
+
+// function add(num1,num2,num3)
+// {
+//     return num1+ num2+ num3
+// }
+// let result = add (5,6,7)
+// console.log(result);
+
+// let greet = function(){
+//     console.log("Hello World");
+//     return 1;
+// }
+
+// console.log(greet()); 
+
+
+// ------------------------------Arrow Functions---------------------------------
+
+// let add = (num1,num2) => num1+num2;
+// let result = add(5,6)
+// console.log(result);
+
+// let input1 = Math.abs(5);
+// let input2 = Math.abs(-7);
+
+// let sum = (num1,num2) => num1 + num2;
+// let result = sum(input1,input2);
+// console.log(result);
+
+// ---------------------------------------This Keyword--------------------------------------------
+ 
+let laptop1 = {
+    cpu :'i7',
+    ram : '16',
+    brand : 'HP',
+    getConfig: function(){
+        console.log(this.cpu);
+    }
+
 }
-let sum = add
-let result =sum(5,5)
-console.log(result);
+
+laptop1.getConfig();
